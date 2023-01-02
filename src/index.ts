f0bcd4ab624853027c88cfbd148dca97f5951e53
devfaoed <faithadedokun2018@gmail.com>
2023-01-02 08:05:08 -0800

    attending to errors in the previous code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import { app } from './app';
+import path from 'path';
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import routes from './router';
 
-const PORT: number = 4000;
+const PORT = process.env.PORT || 3000;
+// Instance of express App
+const app: Application = express();
+// Set Views template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// Morgan Logger Middleware
+app.use(morgan('short'));
+// Routes
+app.use('/', routes);
 
-// Start server
-app.listen(PORT, () =>
-  console.log(`Server started on http://localhost:${PORT}`)
-);
+// Start the Server
+app.listen(PORT, (): void => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
+
+export default app;
--- src/middleware/logger.ts
+++
@@ -1,3 +0,0 @@
-import morgan from 'morgan';
-
-export const morganMiddleware = morgan('short');
